0f13bd5be756f8977436762051290142da92b7cb
Azim Emran <azem.after.graduate@gmail.com>
2023-04-05 07:56:01 +0800

    New files#

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+
+import Introduction from "../Components/Introduction"
+import Navbar from "../Components/Navbar"
+
+const Homepage = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Introduction/>
+            <div style={{height: "600px"}}></div>
+        </div>
+    )
+}
+
+export default Homepage
