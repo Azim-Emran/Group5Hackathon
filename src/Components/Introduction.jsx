bdf7a65d338f60ab4b10141824dadfa41573a3cb
Azim Emran <azem.after.graduate@gmail.com>
2023-04-05 08:16:46 +0800

    Introduction component and loginForm removal from the CSS

---
+++ src/Components/Introduction.jsx
@@ -0,0 +1,17 @@
+const Introduction = () =>{
+
+    return (
+        <div className="wrapper homepage-container row">
+            <div className="col-6">
+                Image
+            </div>
+            <div className="col-6">
+                List
+            </div>
+        </div>
+    )
+
+}
+
+export default Introduction
+
