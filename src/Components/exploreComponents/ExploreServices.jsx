835ee0443e627ff8842ecbe0cfbc66de0f59bcf6
Joey Wong <heyjoeywong@gmail.com>
2023-04-06 20:06:45 +0800

    fixed fetch all

--- src/Components/exploreComponents/ExploreServices.jsx
+++ src/Components/exploreComponents/ExploreServices.jsx
@@ -1,24 +1,27 @@
 import { Card, Button, Image } from "react-bootstrap";
 import { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 const ExploreServices = () => {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [services, setServices] = useState([]);
 
   const fetchServices = async () => {
-    let sql = "SELECT * FROM service_post";
+    let sql;
 
-    if (selectedCategory !== "All") {
-      sql = +`WHERE service_category = '${selectedCategory}`;
+    if (selectedCategory === "All") {
+      sql = "SELECT * FROM service_post";
+    } else {
+      sql = `SELECT * FROM service_post WHERE service_category = '${selectedCategory}'`;
     }
 
     const response = await fetch(
       `https://us-central1-upnxt-fc294.cloudfunctions.net/api/sp?query=${sql}`
     );
-    const services = await response.json();
+    const data = await response.json();
 
-    setServices(services);
+    setServices(data);
+    console.log("Services:", services);
   };
 
   useEffect(() => {
@@ -41,7 +44,10 @@ const ExploreServices = () => {
           >
             Graphic Design
           </Button>
-          <Button class="btn btn-outline-primary mr-3">
+          <Button
+            class="btn btn-outline-primary mr-3"
+            onClick={() => setSelectedCategory("photovideo")}
+          >
             Photography & Videography
           </Button>
           <Button
@@ -50,21 +56,25 @@ const ExploreServices = () => {
           >
             Writing & Translation
           </Button>
-          <Button class="btn btn-outline-primary mr-3">
+          <Button
+            class="btn btn-outline-primary mr-3"
+            onClick={() => setSelectedCategory("programtech")}
+          >
             Programming & Tech
           </Button>
         </div>
 
         <div>
           <ul>
-            {services.map((service) => (
-              <li key={service.service_post_id}>
-                <h2>{service.service_name}</h2>
-                <p>{service.service_description}</p>
-                <p>{service.service_price}</p>
-                <p>{service.service_category}</p>
-              </li>
-            ))}
+            {services.data &&
+              services.data.map((service) => (
+                <li key={service.service_post_id}>
+                  <h2>{service.service_name}</h2>
+                  <p>{service.service_description}</p>
+                  <p>{service.service_price}</p>
+                  <p>{service.service_category}</p>
+                </li>
+              ))}
           </ul>
         </div>
       </div>
