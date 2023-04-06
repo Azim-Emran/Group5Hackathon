81eb5b3e9915ec44d52cad1c33e5f57c1a45ab7e
Alifzaki23 <Alifliplif@gmail.com>
2023-04-07 06:16:33 +0800

    deleted the component

--- src/Components/HJumbotron.jsx
+++ src/Components/HJumbotron.jsx
@@ -1,19 +1,19 @@
-const Jumbotron = () => {
-  return (
-    <div>
-      <div className="row d-flex justify-content-between">
-        <div className="col-12">
-          <h1>
-            WE PROVIDE YOU WITH OUR MULTIPLE TALENTS WHICH CAN BE USED FOR YOUR
-            PROJECT.
-          </h1>
-        </div>
-        <div>
+// const Jumbotron = () => {
+//   return (
+//     <div>
+//       <div className="row d-flex justify-content-between">
+//         <div className="col-12">
+//           <h1>
+//             WE PROVIDE YOU WITH OUR MULTIPLE TALENTS WHICH CAN BE USED FOR YOUR
+//             PROJECT.
+//           </h1>
+//         </div>
+//         <div>
 
-        </div>
-      </div>
-    </div>
-  );
-};
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Jumbotron;
+// export default Jumbotron;
