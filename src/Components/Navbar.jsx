de9505e261e4ac5c50b59b28c11512aa5d108ecb
Azim Emran <azem.after.graduate@gmail.com>
2023-04-06 20:59:18 +0800

    SOme edit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            {userId && <Link className="nav-link" to="/profile">Profile <span className="sr-only">(current)</span></Link>}
+                            {userId && <Link className="nav-link" to="/profile/:userId">Profile <span className="sr-only">(current)</span></Link>}
                         </li>
                     </ul>
                     {userId ? (
